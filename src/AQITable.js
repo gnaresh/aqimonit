4af611d9546ad2f664770e2f63d4d6e439a6896e
Naresh Ghanate <naresh.itian@gmail.com>
2021-05-21 20:12:39 +0530

    AQI monitoring

---
+++ src/AQITable.js
@@ -0,0 +1,147 @@
+import React from "react";
+import "./AQITable.css";
+import moment from 'moment';
+import LiveChart from './LiveChart';
+import AQIGUIDE from './AQIGUIDE.png';
+
+function getColor(aqi) {
+    let classObj = {
+        color: null,
+        level: null,
+        healthImplication: null,
+        CautionaryStatement: null
+    };
+    if (aqi >= 0 && aqi <= 50) {
+        return {
+            color: "#55A850",
+            level: "Good",
+        }
+    } else if (aqi > 50 && aqi < 100) {
+        return {
+            color: "#A3C853",
+            level: "Satisfactory",
+        }
+    } else if (aqi > 100 && aqi <= 200) {
+        return {
+            color: "#EFE71B",
+            level: "Moderate",
+        }
+    }
+    else if (aqi > 200 && aqi <= 300) {
+        return {
+            color: "#F29C33",
+            level: "Poor",
+        }
+    } else if (aqi > 300 && aqi <= 400) {
+        return {
+            color: "#E93F33",
+            level: "Very Poor",
+        }
+    } else if (aqi > 400) {
+        return {
+            color: "#AF2E24",
+            level: "Severe",
+        }
+    }
+    return classObj
+}
+
+class AQITable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataFromServer: {},
+            selectedCity: ""
+        };
+        this.selectCity = this.selectCity.bind(this);
+    }
+    // instance of websocket connection as a class property
+    ws = new WebSocket('ws://city-ws.herokuapp.com/')
+
+    componentDidMount() {
+        this.ws.onopen = () => {
+            // on connecting, do nothing but log it to the console
+            console.log('connected')
+        }
+
+        this.ws.onmessage = evt => {
+            // listen to data sent from the websocket server
+            const message = JSON.parse(evt.data)
+            let { dataFromServer } = this.state;
+            Object.keys(dataFromServer).forEach(x => dataFromServer[x].changed = false);
+            message.map(obj => {
+                if (dataFromServer[obj.city]) {
+                    if (dataFromServer[obj.city].aqi !== obj.aqi) {
+                        if (dataFromServer[obj.city].aqi <= 200 && obj.aqi > 200) { //from good to poor
+                            obj.changed = true
+                        }
+                        dataFromServer[obj.city]["updated"] = moment().valueOf();
+                        dataFromServer[obj.city] = obj
+                    }
+                } else {
+                    dataFromServer[obj.city] = obj
+                    dataFromServer[obj.city]["updated"] = moment().valueOf();
+                }
+            })
+            this.setState({ dataFromServer })
+            // console.log(message)
+        }
+
+        this.ws.onclose = () => {
+            console.log('disconnected')
+            // automatically try to reconnect on connection loss
+
+        }
+    }
+    selectCity(selectedCity) {
+        this.setState({ selectedCity });
+    }
+
+    render() {
+        const { dataFromServer, selectedCity } = this.state;
+        return (
+            <div style={{ margin: 32, textAlign: 'left' }}>
+                <p>Click on a city to view Real Time AQI Chart!</p>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>City</th>
+                                <th>AQI</th>
+                                <th>Last updated/changed</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.keys(dataFromServer).length > 0 ? Object.values(dataFromServer).map((row, r) => {
+
+                                return (
+                                    <tr key={r} id={`row${r}`} onClick={() => this.selectCity(row.city)} >
+                                        <td id={`city${r}`}>
+                                            {row.city}
+                                        </td>
+                                        <td className="aqi" id={`aqi${r}`} style={{ color: getColor(row.aqi).color, background: row.changed === true ? "#F6CED2" : "none" }}>
+                                            {row.aqi.toFixed(2)}
+                                        </td>
+                                        <td id={`date${r}`}>
+                                            {row["updated"] > moment().subtract(1, 'minute').valueOf() ? moment(row["updated"]).fromNow() : moment(row["updated"]).format("HH:mm a")}
+                                        </td>
+                                    </tr>
+                                );
+                            }) :
+                                <tr>No data available yet!</tr>
+                            }
+                        </tbody>
+                    </table>
+                    {
+                        (selectedCity && dataFromServer[selectedCity]) && <LiveChart city={selectedCity} data={dataFromServer[selectedCity]} />
+                    }
+                </div>
+                <div>
+                    <img src={AQIGUIDE} width={480} alt="AQI Guide" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AQITable;
---
+++ src/LiveChart.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import {
+    Line,
+    LineChart,
+    XAxis,
+    YAxis,
+    Tooltip
+} from 'recharts';
+import moment from 'moment';
+
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltip">
+                <span className="label">AQI: {`${payload[0].value.toFixed(2)}`}</span>
+                <br />
+                <span className="desc">Time: {`${moment(label).format("HH:mm:ss a")}`}</span>
+            </div>
+        );
+    }
+    return null;
+};
+const LiveChart = (props) => {
+    const [data, setData] = useState([props.data]);
+    const [city, setCity] = useState([props.city]);
+
+    // 1. listen for a cpu event and update the state
+    useEffect(() => {
+        let arr = [...data, props.data];
+
+        setData(arr.slice(Math.max(arr.length - 10, 0)));
+    }, [props.data]);
+
+    useEffect(() => {
+        setCity(props.city);
+        setData([props.data]);
+    }, [props.city]);
+
+    // 2. render the line chart using the state
+    return (
+        <div>
+            <h3>Real Time AQI ({city})</h3>
+            <LineChart width={500} height={400} data={data}>
+                <XAxis
+                    dataKey="updated"
+                    tickFormatter={(timestamp) => moment(timestamp).format("HH:mm:ss a")}
+                    type='number'
+                    domain={['auto', 'auto']}
+                    scale="time"
+                    angle={-45}
+                    height={100}
+                    textAnchor="end"
+                />
+                <YAxis />
+                <Tooltip content={<CustomTooltip />} />
+                <Line dataKey="aqi" />
+            </LineChart>
+        </div>
+    );
+};
+
+export default LiveChart;
